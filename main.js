bb7e4b3ca596dd794ea1aef7760040ce2aaf368b
419hass <69584970+419hass@users.noreply.github.com>
2022-11-03 21:14:22 +1000

    created board function

---
+++ main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let sketchborder = document.querySelector(".sketch-border");
+    sketchborder.style.gridTemplateColumns = "repeat(15, 1fr)";
+    sketchborder.style.gridTemplateRows = "repeat(15, 1fr)";
+});
